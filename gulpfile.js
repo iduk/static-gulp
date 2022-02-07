94cbe0a23b045b04e753f4f7f0552ab560f041b9
iduk <iduk.design@gmail.com>
2022-02-07 21:03:32 +0900

    [feat] Static 작업환경완료

--- gulpfile.js
+++ gulpfile.js
@@ -5,19 +5,19 @@ const babel = require('gulp-babel')
 const concat = require('gulp-concat')
 const uglify = require('gulp-uglify')
 const rename = require('gulp-rename')
+const sass = require('gulp-sass')(require('sass'))
+const Fiber = require('fibers')
 const postcss = require('gulp-postcss')
-const cssmin = require('gulp-cssmin')
 const autoprefixer = require('autoprefixer')
 const cssnano = require('cssnano')
 const nested = require('postcss-nested')
-const sugarss = require('sugarss')
+const cssnext = require('postcss-cssnext')
 const maps = require('gulp-sourcemaps')
 const del = require('del')
-const browserSync = require('browser-sync').create()
 const imagemin = require('gulp-imagemin')
 const copy = require('gulp-copy')
 const extender = require('gulp-html-extend')
-const sass = require('gulp-sass')(require('sass'))
+const browserSync = require('browser-sync').create()
 
 const PATH = {
   root: 'src/',
@@ -41,56 +41,52 @@ const PATH_DEST = {
 /* 
   Template Pages 
 */
-gulp.task('extend', function () {
+function HtmlExtend() {
   return gulp
     .src(PATH.root + '*.html')
     .pipe(extender({ annotations: true, verbose: false }))
     .pipe(gulp.dest(PATH_DEST.root))
-})
+    .pipe(browserSync.stream())
+}
 
 /* JS - libraries */
-gulp.task('libScripts', function () {
+function jsLint() {
   return gulp
     .src([PATH.ASSETS.js + '*.js'])
     .pipe(uglify())
     .pipe(gulp.dest(PATH_DEST.ASSETS.js))
-})
+    .pipe(browserSync.stream())
+}
 
 /* JS - custom */
-gulp.task('customScripts', function () {
+// gulp.task("customScripts", function () {
+//   return gulp
+//     .src([PATH.ASSETS.js + "*.js"])
+//     .pipe(maps.init())
+//     .pipe(concat("main.js"))
+//     .pipe(uglify())
+//     .pipe(rename({ suffix: ".min" }))
+//     .pipe(maps.write("./maps"))
+//     .pipe(gulp.dest(PATH_DEST.ASSETS.js))
+// })
+
+/*  SASS  */
+function Sass() {
+  let plugins = [nested(), cssnano(), autoprefixer({})]
   return gulp
-    .src([PATH.ASSETS.js + '*.js'])
+    .src(PATH.ASSETS.css + '**/*.scss')
     .pipe(maps.init())
-    .pipe(concat('main.js'))
-    .pipe(uglify())
-    .pipe(rename({ suffix: '.min' }))
-    .pipe(maps.write('.'))
-    .pipe(gulp.dest(PATH_DEST.ASSETS.js))
-})
-
-/* 
-  SCSS/CSS 
-*/
-
-gulp.task('compileSass', function () {
-  // let plugins = [nested]
-  return (
-    gulp
-      .src([PATH.ASSETS.css + '**/*.scss'])
-      .pipe(maps.init())
-      .pipe(sass().on('error', sass.logError))
-      // 오류 node-sass 설치해봐야됨
-      // .pipe(postcss(plugins, { parser: sugarss }))
-      .pipe(maps.write('.'))
-      .pipe(gulp.dest(PATH_DEST.ASSETS.css))
-      .pipe(browserSync.stream({ match: '**/*.css' }))
-  )
-})
+    .pipe(sass({ fiber: Fiber }).on('error', sass.logError))
+    .pipe(postcss(plugins))
+    .pipe(maps.write('./map'))
+    .pipe(gulp.dest(PATH_DEST.ASSETS.css))
+    .pipe(browserSync.stream())
+}
 
 /* 
   Image Compress 
 */
-gulp.task('compressImages', function () {
+function ImgMin() {
   return gulp
     .src([PATH.ASSETS.img + '*'])
     .pipe(
@@ -114,70 +110,39 @@ gulp.task('compressImages', function () {
       ])
     )
     .pipe(gulp.dest(PATH_DEST.ASSETS.img))
-})
+}
 
-/* 
-  Copy Fonts 
-*/
-gulp.task('copy', function () {
+/* Copy Fonts */
+function CopyFont() {
   return gulp.src(PATH.ASSETS.fonts).pipe(gulp.dest(PATH_DEST.ASSETS.fonts))
-})
+}
 
-gulp.task('clean', function (done) {
+/* Clean */
+function Clean(done) {
   del([PATH_DEST.root]), done()
-})
+}
 
-/* 
-  BUILD START
-*/
+/* Server */
+function reload(done) {
+  browserSync.reload()
+  done()
+}
 
-gulp.task(
-  'build',
-  gulp.series(
-    'extend',
-    'copy',
-    'libScripts',
-    'customScripts',
-    'compileSass',
-    'compressImages'
-  ),
-  function () {
-    return gulp
-      .src(
-        [
-          PATH_DEST.root + '**/*.html',
-          PATH_DEST.root + '**/*.ico',
-          PATH_DEST.fonts + '**/*.*',
-          PATH_DEST.ASSETS.img + '**/*.*',
-        ],
-        {
-          base: './',
-        }
-      )
-      .pipe(babel())
-      .pipe(gulp.dest(PATH_DEST.root))
-  }
-)
-
-/*
-  WATCH
-*/
-gulp.task('watchFiles', function () {
+function watch() {
   browserSync.init({
-    server: PATH_DEST.root,
-    baseDir: './',
-    open: true,
-  }),
-    gulp.watch(PATH.ASSETS.css + '**/*.scss', gulp.series('compileSass'))
-  gulp.watch(
-    PATH.ASSETS.js + '**/*.js',
-    gulp.series('libScripts', 'customScripts')
-  )
-  gulp.watch(PATH.root + '**/*.html', gulp.series('extend'))
-  gulp.watch('**/*.html').on('change', browserSync.reload)
-})
+    server: {
+      baseDir: PATH_DEST.root,
+    },
+  })
+  gulp.watch(PATH.ASSETS.img, ImgMin)
+  gulp.watch(PATH.ASSETS.css + '**/*.scss', Sass)
+  gulp.watch(PATH.root + '**/*.html', HtmlExtend)
+  gulp.watch(PATH.root + '**/*.js', jsLint)
+  gulp.watch(PATH.root + '**/*.html', reload)
+}
+exports.watch = watch
 
-/* gulp default */
-gulp.task('default', gulp.parallel('clean', 'build', 'watchFiles'))
-/* gulp dev */
-gulp.task('dev', gulp.parallel('clean', 'build'))
+const Build = gulp.parallel(HtmlExtend, Sass, jsLint, ImgMin, CopyFont, watch)
+
+gulp.task('default', Build)
+gulp.task('clean', Clean)
